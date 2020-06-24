cb1c2b0c8f8aa9bbc02c6cee9811604a63d1c0f0
Andrew Ferrier <andrewferrier@users.noreply.github.com>
2020-06-24 17:03:53 +0100

    Update bootstrap.sh
    Install git if needed.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if command -v apt-get; then
+  if ! command -v git; then
+    apt-get update && apt-get install -y git
+  fi
+fi
+
 cd ~
 git clone https://github.com/andrewferrier/dotfiles-basic.git
 cd dotfiles-basic
